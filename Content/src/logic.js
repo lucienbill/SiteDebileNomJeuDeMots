29e92cea88ed34ec454bab319ebe03fd6c728ea1
hugo <hugo.lm2707@gmail.com>
2023-08-09 00:10:13 +0200

    add history display + navigation

--- Content/src/logic.js
+++ Content/src/logic.js
@@ -1,6 +1,11 @@
 // --- Global variables ---
 // set timer identifier for the thing that handles how the copy button looks
 let timerID = null;
+// save the previous names in an array
+const nameHistory = [];
+// paging stuff for history
+let historyPage = 1;
+const historyPageSize = 10;
 // Stuff for the thing that handles how the copy button looks
 const copyButtonLooks = {
     "copy"   : "📄",
@@ -12,13 +17,47 @@ const copyButtonTitle = {
 }
 // --- Functions ---
 function printARandomName() {
-    const fname = document.getElementById("fname")
+    const fname = document.getElementById("fname");
     
     if (document.getElementById("allowNSFW").checked){ //NSFW
         fname.value = allnames[Math.floor(Math.random() * allnames.length)];
     } else { //SFW
         fname.value = namesSFW[Math.floor(Math.random() * namesSFW.length)];
     }
+
+    // Add the name at the start of the history then redraw it
+    nameHistory.unshift(fname.value);
+    drawHistory();
+}
+
+function drawHistory() {
+
+    // Get DOM els
+    const history = document.getElementById("history");
+    const historyCount = document.getElementById("historyCount");
+    const historyNav = document.getElementById("historyNav");
+
+    // Reset els
+    historyCount.innerHTML = nameHistory.length;
+    history.innerHTML = "";
+    historyNav.innerHTML = "";
+
+    // Get names depending on current page
+    const [start, end] = [(historyPage - 1) * historyPageSize, historyPage * historyPageSize];
+    const names = nameHistory.slice(start, end)
+
+    // Map the content
+    history.innerHTML = names.map(name => "<li>" + name + "</li>").join('');
+    const pagesCount = Math.ceil(nameHistory.length / historyPageSize);
+    for (p = 1; p <= pagesCount; p++) {
+        historyNav.innerHTML += "<li" + (p === historyPage ? " class=\"current\"" : "") + " onclick=\"showHistoryPage(" + p + ")\">" + p + "<li>"
+    }
+}
+function showHistoryPage(page) {
+    if (page === historyPage) { return; }
+    const pagesCount = Math.ceil(nameHistory.length / historyPageSize);
+    historyPage = page <= pagesCount ? page : pagesCount;
+    drawHistory();
 }
 
 function copyToClipboard() {
@@ -48,7 +87,6 @@ function copyToClipboard() {
         , 2000
     )
 }
-
 // --- Do stuff ---
 // adding "nsfw" as a get parameter checks the checkbox (example: mysite.dev/?nsfw)
 if (window.location.search.substr(1).split("&").includes("nsfw")) {
